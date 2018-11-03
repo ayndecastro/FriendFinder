2e7f92acf6f771eb25bca080fd0be85ea35bc58e
ayn <vincentayndecastro@gmail.com>
2018-11-03 00:11:09 -0500

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,9 +4,9 @@ let friendsSurvey = [
         name: "Ayn",
         image: "https://thumbs.dreamstime.com/z/portrait-pig-glasses-cute-white-background-38945684.jpg",
         answer: [
+            "1",
             "5",
-            "5",
-            "5",
+            "3",
             "5",
             "5",
             "5",
@@ -21,13 +21,13 @@ let friendsSurvey = [
         image: "https://pbs.twimg.com/profile_images/1046968391389589507/_0r5bQLl_400x400.jpg",
         answer: [
             "1",
+            "4",
             "1",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
+            "5",
+            "4",
+            "5",
+            "5",
             "1",
             "1"
             ]
@@ -37,15 +37,15 @@ let friendsSurvey = [
         image: "http://propeller.hu/images/cikk/shutterstock_143589274-2_664x373t5_ic.jpg",
         answer: [
             "1",
+            "5",
+            "5",
+            "5",
             "1",
+            "5",
+            "5",
+            "3",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1"
+            "5"
             ]
     }
     
